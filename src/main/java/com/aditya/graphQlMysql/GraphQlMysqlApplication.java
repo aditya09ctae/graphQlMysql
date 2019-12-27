cf2064bef1f2eb9bd797e0a4445f891b2b583fb3
aditya09ctae <aditya.ctae@gmail.com>
2019-12-27 13:30:37 +0530

    project started

---
+++ src/main/java/com/aditya/graphQlMysql/GraphQlMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.graphQlMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlMysqlApplication.class, args);
+	}
+
+}
